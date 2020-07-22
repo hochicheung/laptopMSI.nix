3902c2d5f61e1724c25725865f6837b4e00cf865
samtex98 <hoche002@gmail.com>
2020-07-22 21:59:31 +0000

    init file init

---
+++ configuration.nix
@@ -0,0 +1,56 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  # Use the systemd-boot EFI boot loader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  # networking.hostName = "nixos"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # The global useDHCP flag is deprecated, therefore explicitly set to false here.
+  # Per-interface useDHCP will be mandatory in the future, so this generated config
+  # replicates the default behaviour.
+  networking.useDHCP = false;
+  networking.interfaces.enp3s0.useDHCP = true;
+  networking.interfaces.wlp2s0.useDHCP = true;
+
+  # Networking
+  networking.networkmanager.enable = true;
+
+  # User Management
+  users.users.samcheung = {
+    isNormalUser = true;
+    home = "/home/samcheung";
+    extraGroups = ["wheel" "networkmanager"];
+  };
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "20.03"; # Did you read the comment?
+  
+  # Packages
+  environment.systemPackages = [pkgs.git
+				pkgs.vim
+				pkgs.emacs]; 
+ 
+  # X11
+  services.xserver.enable = true;
+  services.xserver.layout = us;
+  services.xserver.xkbVariant = altgr-intl;
+  services.xserver.xkbOptions = caps:escape; 
+}
+
