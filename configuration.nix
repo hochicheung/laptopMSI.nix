90b076f6a587e451cce5a27a18cc36da606576e3
samtex98 <hoche002@gmail.com>
2020-08-10 16:00:37 +0200

    moved variables to a hardware-local file

--- configuration.nix
+++ configuration.nix
@@ -6,26 +6,22 @@
 
 {
   imports =
-    [ # Include the results of the hardware scan.
+    [ 
       ./hardware-configuration.nix
+			./laptopMSI.nix
     ];
 
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  # networking.hostName = "nixos"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # The global useDHCP flag is deprecated, therefore explicitly set to false here.
-  # Per-interface useDHCP will be mandatory in the future, so this generated config
-  # replicates the default behaviour.
-  networking.useDHCP = false;
-  networking.interfaces.enp3s0.useDHCP = true;
-  networking.interfaces.wlp2s0.useDHCP = true;
-
+	# Time & Date
+	time.timeZone = "Europe/Stockholm";
+	services.localtime.enable = true;
+  
   # Networking
   networking.networkmanager.enable = true;
+  networking.useDHCP = false;
 
   # User Management
   users.users.samcheung = {
@@ -34,31 +30,23 @@
     extraGroups = ["wheel" "networkmanager"];
   };
 
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "20.03"; # Did you read the comment?
   
   # Packages
-  environment.systemPackages = [pkgs.git
+  environment.systemPackages = [
+				pkgs.git
 				pkgs.vim
 				pkgs.emacs
-				pkgs.firefox]; 
+				pkgs.keepassxc
+	]; 
 
-  # Touchpad
-  services.xserver.libinput.enable = true;
-  services.xserver.libinput.tapping = false;
- 
-  # X11
-  services.xserver.enable = true;
-  services.xserver.layout = "us";
-  services.xserver.xkbVariant = "altgr-intl";
-  services.xserver.xkbOptions = "caps:escape"; 
-
+	# X Window-manager
   services.xserver.autorun = false;
   services.xserver.windowManager.exwm.enable = true;
+	services.xserver.desktopManager.xterm.enable = true;
+
+	system.autoUpgrade.enable = true;
+	system.autoUpgrade.allowReboot = true;
+
+  system.stateVersion = "20.03";
 }
 
